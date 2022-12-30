9702153c75449a2928a159f2d1daab2e8328a388
Cesar Rodriguez <cesar.rodriguez.lara54@gmail.com>
2022-12-30 02:43:55 -0600

    Get user prompt

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    let mut coin: String = String::new();
+    let result = std::io::stdin().read_line(&mut coin);
+    match result {
+        Ok(_) => {
+            let currency = get_currency_price(&coin);
+            println!("El precio es: {}", currency);
+        }
+        Err(error) => println!("Error: {}", error),
+    }
+}
+
+fn get_currency_price(_coin: &str) -> String {
+    String::from("Test")
 }
